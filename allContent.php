891125407376c4bfe05e96c71545dd5cba79f702
usmetanina <usmetanina96@mail.ru>
2016-09-29 15:54:11 +0500

    оптимизирован массив

--- allContent.php
+++ allContent.php
@@ -1,39 +1,36 @@
 <?php
 class AllContent {
 	static $items = array(
-			1=>array('id'=>1,
-			'title'=>'Start',
-			'href'=>'index.php?page=1',
-			'class'=>'mainmenu__items',
-			'content'=>'.\assets\start.html'),
+			1=>array('title'=>'Start',
+			'class'=>'mainmenu__item',
+			'content'=>'./assets/start.html'),
 	
-			2=>array('id'=>2,
-			'title'=>'About',
-			'href'=>'index.php?page=2',
-			'class'=>'mainmenu__items',
-			'content'=>".\assets\about.html"),
+			2=>array('title'=>'About',
+			'class'=>'mainmenu__item',
+			'content'=>"./assets/about.html"),
 	
-			3=>array('id'=>3,
-			'title'=>'Top list',
-			'href'=>'index.php?page=3',
-			'class'=>'mainmenu__items',
-			'content'=>'.\assets\toplist.html') 
+			3=>array('title'=>'Top list',
+			'class'=>'mainmenu__item',
+			'content'=>'./assets/toplist.html') 
 	);
 
 	public static function getMenu($numberPage) {
-		if ($numberPage>0&&$numberPage<4)
-			self::$items[$numberPage]["class"]="mainmenu__items__active";
+		if (array_key_exists ($numberPage,self::$items))
+			self::$items[$numberPage]["class"]="mainmenu__item mainmenu__item__active";
 	
 		$resultMenu="<ul class='mainmenu'>";
-
+		
+		$k=key(self::$items);
 		foreach (self::$items as $item ){
-			$resultMenu=$resultMenu."<li class = $item[class]> <a href= $item[href]> $item[title] </a> </li>";
+			$resultMenu=$resultMenu."<li class = '$item[class]'> <a href=index.php?page=$k> $item[title] </a> </li>";
+			$k=key(self::$items);
+			next(self::$items);
 		}
 		return $resultMenu.'</ul>';
 	}
 	
 	public static function getContent($numberPage) {
-		if ($numberPage>0&&$numberPage<4)
+		if (array_key_exists ($numberPage,self::$items))
 		return file_get_contents(self::$items[$numberPage]["content"]);
 	}
 	
